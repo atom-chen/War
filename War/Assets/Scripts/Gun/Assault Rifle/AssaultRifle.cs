c2b49006316fe1dafd93c1d42118a1a45dd4175b
NiHanGame <371429357@qq.com>
2019-11-14 21:00:11 +0800

    22 主枪械使用DOTween优化开镜动作.

--- War/Assets/Scripts/Gun/Assault Rifle/AssaultRifle.cs
+++ War/Assets/Scripts/Gun/Assault Rifle/AssaultRifle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 /// <summary>
 /// 突击步枪C层.
@@ -9,10 +10,18 @@ public class AssaultRifle : MonoBehaviour
 {
     private Transform m_Transform;
     private Animator m_Animator;
+    private Camera m_EnvCamera;
+
+    // 枪械开镜动作优化.
+    private Vector3 startPos;
+    private Vector3 startRot;
+    private Vector3 endPos;
+    private Vector3 endRot;
 
     void Start()
     {
         FindAndLoadInit();
+        InitHoldPose();
     }
 
     void Update()
@@ -27,12 +36,14 @@ public class AssaultRifle : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             m_Animator.SetBool("HoldPose", true);
+            EnterHoldPose();
         }
 
         // 松开鼠标右键退出开镜.
         if (Input.GetMouseButtonUp(1))
         {
             m_Animator.SetBool("HoldPose", false);
+            ExitHoldPose();
         }
     }
 
@@ -43,5 +54,39 @@ public class AssaultRifle : MonoBehaviour
     {
         m_Transform = gameObject.GetComponent<Transform>();
         m_Animator = gameObject.GetComponent<Animator>();
+        m_EnvCamera = GameObject.Find("FPSController/EnvCamera").GetComponent<Camera>();
+    }
+
+    /// <summary>
+    /// 初始化开镜动作.
+    /// </summary>
+    private void InitHoldPose()
+    {
+        startPos = m_Transform.localPosition;
+        startRot = m_Transform.localRotation.eulerAngles;
+        endPos = new Vector3(-0.065f, -1.85f, 0.25f);
+        endRot = new Vector3(2.8f, 1.3f, 0.08f);
+    }
+
+    /// <summary>
+    /// 进入开镜状态.
+    /// </summary>
+    private void EnterHoldPose(float time = 0.2f, float fov = 40.0f)
+    {
+        m_Transform.DOLocalMove(endPos, time);
+        m_Transform.DOLocalRotate(endRot, time);
+
+        m_EnvCamera.DOFieldOfView(fov, time);
+    }
+
+    /// <summary>
+    /// 退出开镜状态.
+    /// </summary>
+    private void ExitHoldPose(float time = 0.2f, float fov = 60.0f)
+    {
+        m_Transform.DOLocalMove(startPos, time);
+        m_Transform.DOLocalRotate(startRot, time);
+
+        m_EnvCamera.DOFieldOfView(fov, time);
     }
 }
