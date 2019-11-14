baa0012b8cf805f890babc6298bbb6c39c52147a
NiHanGame <371429357@qq.com>
2019-11-14 19:59:58 +0800

    20 角色武器切换模拟.

--- War/Assets/Scripts/Player/PlayerController.cs
+++ War/Assets/Scripts/Player/PlayerController.cs
@@ -8,21 +8,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Transform m_Transform;
-    private Animator m_Animator;
 
     private GameObject m_BuildingPlan;              // 建造角色.
+    private GameObject m_WoodenSpear;               // 长矛角色.
 
-    private bool isNormal = false;                  // T: 空手; F: 建造.
+    private GameObject currentWeapon;               // 当前角色.
+    private GameObject targetWeapon;                // 目标角色.
 
     void Start()
     {
-        FindAndLoadInnit();
+        FindAndLoadInit();
     }
 
     void Update()
     {
+        // 按下B建造.
         if (Input.GetKeyDown(KeyCode.B))
         {
+            targetWeapon = m_BuildingPlan;
+            Change();
+        }
+
+        // 按下N长矛.
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            targetWeapon = m_WoodenSpear;
             Change();
         }
     }
@@ -30,12 +40,15 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// 查找加载初始化.
     /// </summary>
-    private void FindAndLoadInnit()
+    private void FindAndLoadInit()
     {
         m_Transform = gameObject.GetComponent<Transform>();
 
         m_BuildingPlan = m_Transform.Find("EnvCamera/PersonCamera/Building Plan").gameObject;
-        m_Animator = m_BuildingPlan.GetComponent<Animator>();
+        m_WoodenSpear = m_Transform.Find("EnvCamera/PersonCamera/Wooden Spear").gameObject;
+
+        currentWeapon = m_BuildingPlan;
+        m_WoodenSpear.SetActive(false);
     }
 
     /// <summary>
@@ -43,17 +56,9 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void Change()
     {
-        if (isNormal)
-        {
-            m_BuildingPlan.SetActive(true);
-        }
-        else
-        {
-            m_Animator.SetTrigger("Holster");
-            StartCoroutine("DelayToHide");
-        }
+        currentWeapon.GetComponent<Animator>().SetTrigger("Holster");
 
-        isNormal = !isNormal;
+        StartCoroutine("DelayToHide");
     }
 
     /// <summary>
@@ -62,6 +67,9 @@ public class PlayerController : MonoBehaviour
     private IEnumerator DelayToHide()
     {
         yield return new WaitForSeconds(1);
-        m_BuildingPlan.SetActive(false);
+        currentWeapon.SetActive(false);
+        targetWeapon.SetActive(true);
+
+        currentWeapon = targetWeapon;
     }
 }
