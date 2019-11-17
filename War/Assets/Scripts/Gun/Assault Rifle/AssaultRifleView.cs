753d1d8204646bb9321a997a0090df46cc13df90
NiHanGame <371429357@qq.com>
2019-11-17 15:30:40 +0800

    30 枪械类V层框架提取.

--- War/Assets/Scripts/Gun/Assault Rifle/AssaultRifleView.cs
+++ War/Assets/Scripts/Gun/Assault Rifle/AssaultRifleView.cs
@@ -1,26 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 /// <summary>
 /// 突击步枪V层.
 /// </summary>
-public class AssaultRifleView : MonoBehaviour
+public class AssaultRifleView : GunViewBase
 {
-    private Transform m_Transform;
-    private Animator m_Animator;
-    private Camera m_EnvCamera;
-
-    // 枪械开镜动作优化.
-    private Vector3 startPos;
-    private Vector3 startRot;
-    private Vector3 endPos;
-    private Vector3 endRot;
-
-    private Transform gunPoint;                     // 枪口位置.
     private Transform shellPoint;                   // 弹壳弹出位置.
-    private Transform gunStar;                      // 准星UI.
 
     private GameObject prefab_Bullet;               // 子弹预制体(临时).
     private GameObject prefab_Shell;                // 弹壳预制体.
@@ -28,35 +15,18 @@ public class AssaultRifleView : MonoBehaviour
     private Transform effectParent;                 // 特效父物体.
     private Transform shellParent;                  // 弹壳父物体.
 
-    public Transform M_Transform { get => m_Transform; }
-    public Animator M_Animator { get => m_Animator; }
-    public Camera M_EnvCamera { get => m_EnvCamera; }
-    public Transform GunPoint { get => gunPoint; }
     public Transform ShellPoint { get => shellPoint; }
-    public Transform GunStar { get => gunStar; }
     public GameObject Prefab_Bullet { get => prefab_Bullet; }
     public GameObject Prefab_Shell { get => prefab_Shell; }
     public Transform EffectParent { get => effectParent; }
     public Transform ShellParent { get => shellParent; }
 
-    void Awake()
-    {
-        FindAndLoadInit();
-        InitHoldPose();
-    }
-
     /// <summary>
     /// 查找加载初始化.
     /// </summary>
-    private void FindAndLoadInit()
+    protected override void FindAndLoadInit()
     {
-        m_Transform = gameObject.GetComponent<Transform>();
-        m_Animator = gameObject.GetComponent<Animator>();
-        m_EnvCamera = GameObject.Find("FPSController/EnvCamera").GetComponent<Camera>();
-
-        gunPoint = m_Transform.Find("Armature/Weapon/EffectPos_A");
-        shellPoint = m_Transform.Find("Armature/Weapon/EffectPos_B");
-        gunStar = GameObject.Find("TempPanel/GunStar").GetComponent<Transform>();
+        shellPoint = M_Transform.Find("Armature/Weapon/EffectPos_B");
 
         prefab_Bullet = Resources.Load<GameObject>("Gun/Bullet/Bullet");
         prefab_Shell = Resources.Load<GameObject>("Gun/Bullet/Shell");
@@ -68,33 +38,16 @@ public class AssaultRifleView : MonoBehaviour
     /// <summary>
     /// 初始化开镜动作.
     /// </summary>
-    private void InitHoldPose()
+    protected override void InitHoldPose()
     {
-        startPos = m_Transform.localPosition;
-        startRot = m_Transform.localRotation.eulerAngles;
+        startPos = M_Transform.localPosition;
+        startRot = M_Transform.localRotation.eulerAngles;
         endPos = new Vector3(-0.065f, -1.85f, 0.25f);
         endRot = new Vector3(2.8f, 1.3f, 0.08f);
     }
 
-    /// <summary>
-    /// 进入开镜状态.
-    /// </summary>
-    public void EnterHoldPose(float time = 0.2f, float fov = 40.0f)
+    protected override void FindGunPoint()
     {
-        m_Transform.DOLocalMove(endPos, time);
-        m_Transform.DOLocalRotate(endRot, time);
-
-        m_EnvCamera.DOFieldOfView(fov, time);
-    }
-
-    /// <summary>
-    /// 退出开镜状态.
-    /// </summary>
-    public void ExitHoldPose(float time = 0.2f, float fov = 60.0f)
-    {
-        m_Transform.DOLocalMove(startPos, time);
-        m_Transform.DOLocalRotate(startRot, time);
-
-        m_EnvCamera.DOFieldOfView(fov, time);
+        gunPoint = M_Transform.Find("Armature/Weapon/EffectPos_A");
     }
 }
---
+++ War/Assets/Scripts/Gun/Base Class/GunViewBase.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+/// <summary>
+/// 枪械模块V层顶级基类.
+/// </summary>
+public abstract class GunViewBase : MonoBehaviour
+{
+    private Transform m_Transform;
+    private Animator m_Animator;
+    private Camera m_EnvCamera;
+
+    // 枪械开镜动作优化.
+    protected Vector3 startPos;
+    protected Vector3 startRot;
+    protected Vector3 endPos;
+    protected Vector3 endRot;
+
+    protected Transform gunPoint;                       // 枪口位置.
+    private Transform gunStar;                          // 准星UI.
+
+    protected Transform M_Transform { get => m_Transform; }
+    public Animator M_Animator { get => m_Animator; }
+    public Camera M_EnvCamera { get => m_EnvCamera; }
+    public Transform GunPoint { get => gunPoint; }
+    public Transform GunStar { get => gunStar; }
+
+    void Awake()
+    {
+        InitBase(); 
+        FindGunStar();
+
+        FindAndLoadInit();
+        InitHoldPose();
+        FindGunPoint();
+    }
+
+    /// <summary>
+    /// 初始化基类.
+    /// </summary>
+    private void InitBase()
+    {
+        m_Transform = gameObject.GetComponent<Transform>();
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_EnvCamera = GameObject.Find("FPSController/EnvCamera").GetComponent<Camera>();
+    }
+
+    /// <summary>
+    /// 查找准星.
+    /// </summary>
+    private void FindGunStar()
+    {
+        gunStar = GameObject.Find("TempPanel/GunStar").GetComponent<Transform>();
+    }
+
+    /// <summary>
+    /// 进入开镜状态.
+    /// </summary>
+    public void EnterHoldPose(float time = 0.2f, float fov = 40.0f)
+    {
+        m_Transform.DOLocalMove(endPos, time);
+        m_Transform.DOLocalRotate(endRot, time);
+
+        m_EnvCamera.DOFieldOfView(fov, time);
+    }
+
+    /// <summary>
+    /// 退出开镜状态.
+    /// </summary>
+    public void ExitHoldPose(float time = 0.2f, float fov = 60.0f)
+    {
+        m_Transform.DOLocalMove(startPos, time);
+        m_Transform.DOLocalRotate(startRot, time);
+
+        m_EnvCamera.DOFieldOfView(fov, time);
+    }
+
+    /// <summary>
+    /// 查找加载初始化.
+    /// </summary>
+    protected abstract void FindAndLoadInit();
+
+    /// <summary>
+    /// 初始化开镜动作.
+    /// </summary>
+    protected abstract void InitHoldPose();
+
+    /// <summary>
+    /// 查找枪口位置.
+    /// </summary>
+    protected abstract void FindGunPoint();
+}
