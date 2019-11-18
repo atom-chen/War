292985201570142d631ba071d3984dbb9cc8247b
NiHanGame <371429357@qq.com>
2019-11-18 17:21:59 +0800

    38 弓箭、长矛尾部颤动效果实现.

--- War/Assets/Scripts/Gun/Bullet/Arrow.cs
+++ War/Assets/Scripts/Gun/Bullet/Arrow.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 弓箭管理器.
+/// 弓箭、长矛管理器.
 /// </summary>
 public class Arrow : MonoBehaviour
 {
     private Transform m_Transform;
     private Rigidbody m_Rigidbody;
     private BoxCollider m_BoxCollider;
+
+    private Transform m_PivotTransform;         // 中心点用于尾部动画.
 
     private int damage;                         // 武器伤害值.
 
@@ -28,6 +30,8 @@ public class Arrow : MonoBehaviour
         {
             bulletMark.Hp -= damage;
             m_Transform.SetParent(other.gameObject.GetComponent<Transform>());
+
+            StartCoroutine("TailAnimation");
         }
     }
 
@@ -39,6 +43,8 @@ public class Arrow : MonoBehaviour
         m_Transform = gameObject.GetComponent<Transform>();
         m_Rigidbody = gameObject.GetComponent<Rigidbody>();
         m_BoxCollider = gameObject.GetComponent<BoxCollider>();
+
+        m_PivotTransform = m_Transform.Find("Pivot");
     }
 
     /// <summary>
@@ -52,5 +58,32 @@ public class Arrow : MonoBehaviour
         this.damage = damage;
 
         m_Rigidbody.AddForce(dir * force, ForceMode.Impulse);
+    }
+
+    /// <summary>
+    /// 弓箭、长矛尾部动画效果.
+    /// </summary>
+    private IEnumerator TailAnimation()
+    {
+        // 动画结束时间.
+        float stopTime = Time.time + Random.Range(1f, 2f);
+
+        // 动画偏转范围.
+        float range = 1.0f;
+        float arg = 0.0f;
+
+        // 起始旋转.
+        Quaternion startRot = Quaternion.Euler(new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0));
+
+        while (Time.time < stopTime)
+        {
+            m_PivotTransform.localRotation = startRot * Quaternion.Euler(
+                new Vector3(Random.Range(-range, range), Random.Range(-range, range), 0));
+
+            // 平滑阻尼.
+            range = Mathf.SmoothDamp(range, 0, ref arg, stopTime - Time.time);
+
+            yield return null;
+        }
     }
 }
